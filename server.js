30d6c6308cd8961b130038b70a7276ce4991722c
Mohamed Dhia Dhaouadi <73595881+dhia88@users.noreply.github.com>
2023-08-10 19:50:49 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const app = express();
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "john",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "sally",
+      email: "saly@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+app.get("/", (req, res) => {
+  res.send("this is working");
+});
+
+app.post("/signin", (req, res) => {
+  res.send("signedin");
+});
+
+app.listen(3000, () => {
+  console.log("app is running");
+});
