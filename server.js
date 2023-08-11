77be47544ff160412d76ed96d889a2498d0417d9
dhia88 <dhaouadi.mohamed.dhia@hotmail.fr>
2023-08-11 07:54:46 +0100

    profile and image

--- server.js
+++ server.js
@@ -50,7 +50,36 @@ app.post("/register", (req, res) => {
     entries: 0,
     joined: new Date(),
   });
-  res.json(database.users[database.users.length-1]);
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(404).json("no such user");
+  }
+});
+
+app.post("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(404).json("no such user");
+  }
 });
 
 app.listen(3000, () => {
