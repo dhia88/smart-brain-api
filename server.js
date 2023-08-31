518f8d2f54150f71696b49340226262ae064adef
dhia88 <dhaouadi.mohamed.dhia@hotmail.fr>
2023-08-31 05:21:23 -0400

    regeitering a user

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "Dhia@1988",
+    database: "smart-brain",
+  },
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,7 +36,16 @@ const database = {
       joined: new Date(),
     },
   ],
+  login: [
+    {
+      id: "879",
+      hash: "",
+      email: "john@gmail.com",
+    },
+  ],
 };
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(database.users);
@@ -34,7 +56,7 @@ app.post("/signin", (req, res) => {
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
   ) {
-    res.json("sucsess");
+    res.status(200).json(database.users[0]);
   } else {
     res.status(400).json("error logging in");
   }
@@ -42,15 +64,18 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  database.users.push({
-    id: "125",
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date(),
+  bcrypt.hash(password, null, null, function (err, hash) {
+    console.log(hash);
   });
-  res.json(database.users[database.users.length - 1]);
+  db("users")
+    .returning("*")
+    .insert({
+      name: name,
+      email: email,
+      joined: new Date(),
+    })
+    .then((user) => res.json(user[0]))
+    .catch((err) => res.status(400).json('Enable to register'));
 });
 
 app.get("/profile/:id", (req, res) => {
@@ -67,7 +92,7 @@ app.get("/profile/:id", (req, res) => {
   }
 });
 
-app.post("/image", (req, res) => {
+app.put("/image", (req, res) => {
   const { id } = req.body;
   let found = false;
   database.users.forEach((user) => {
@@ -82,6 +107,16 @@ app.post("/image", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+/*
+// Load hash from your password DB.
+bcrypt.compare("bacon", hash, function(err, res) {
+  // res == true
+});
+bcrypt.compare("veggies", hash, function(err, res) {
+  // res = false
+});
+*/
+
+app.listen(3001, () => {
   console.log("app is running");
 });
