0da822d03d809258ecf212c282826ed234bef787
dhia88 <dhaouadi.mohamed.dhia@hotmail.fr>
2023-09-02 19:57:46 -0400

    updating entries

--- server.js
+++ server.js
@@ -98,17 +98,15 @@ app.get("/profile/:id", (req, res) => {
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(404).json("no such user");
-  }
+
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json("Unable do get entries"));
 });
 
 /*
