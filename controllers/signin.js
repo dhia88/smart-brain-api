0c2d21ce5d4cec752056c5f8ae8dc56b463dca29
dhia88 <dhaouadi.mohamed.dhia@hotmail.fr>
2023-09-03 23:39:18 -0400

    input validation

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,14 +1,19 @@
 const handleSignIn = (req, res, db, bcrypt) => {
+  const { email, password } = req.body;
+  if (!password || !email) {
+    return res.status(400).json("incorrect form submission");
+  }
+
   db.select("email", "hash")
     .from("login")
-    .where("email", "=", req.body.email)
+    .where("email", "=", email)
     .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return db
           .select("*")
           .from("users")
-          .where("email", "=", req.body.email)
+          .where("email", "=", email)
           .then((user) => res.json(user[0]))
           .catch((err) => res.status(400).json("Unable to get user"));
       } else {
@@ -19,4 +24,3 @@ const handleSignIn = (req, res, db, bcrypt) => {
 };
 
 export default handleSignIn;
-
